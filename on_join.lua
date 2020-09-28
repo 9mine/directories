26fc16eb1dc0d19afc012e6fe9b50a15f42b12dc
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-28 21:25:03 +0300

    add tool for creating files

--- on_join.lua
+++ on_join.lua
@@ -2,6 +2,6 @@ minetest.register_on_joinplayer(function(player)
     local inventory = player.get_inventory(player)
     nmine.populate_inventory(inventory, "directories:enter",
                              "directories:spawn_rootdir", "directories:read",
-                             "directories:write", "directories:stats")
+                             "directories:write", "directories:stats", "directories:create_file")
     current_hud[player:get_player_name()] = nil
 end)
--- tools.lua
+++ tools.lua
@@ -2,13 +2,8 @@ minetest.register_tool("directories:stats", {
     desription = "Read stats",
     inventory_image = "directories_stats.png",
     wield_image = "directories_stats.png",
-    tool_capabilities = {punch_attack_uses = 0, damage_groups = {stats = 1}},
+    tool_capabilities = {punch_attack_uses = 0, damage_groups = {stats = 1}}
 
-    -- on_use = function(itemstack, player, pointed_thing)
-    --     if pointed_thing.type == "nothing" then
-    --         print("HUD shoud be deleted")
-    --     end
-    -- end
 })
 
 minetest.register_tool("directories:enter", {
@@ -30,6 +25,28 @@ minetest.register_tool("directories:write", {
     inventory_image = "directories_write.png",
     wield_image = "directories_write.png",
     tool_capabilities = {punch_attack_uses = 0, damage_groups = {write = 1}}
+})
+
+minetest.register_tool("directories:create_file", {
+    desription = "Create file",
+    inventory_image = "directories_create_file.png",
+    wield_image = "directories_create_file.png",
+    tool_capabilities = {
+        punch_attack_uses = 0,
+        damage_groups = {create_file = 1}
+    },
+    on_use = function(itemstack, player, pointed_thing)
+        local player_name = player:get_player_name()
+
+        local formspec = {
+            "formspec_version[3]", "size[10,10.5,false]",
+            "field[0.5,0.5;9,1;file_name;Enter File Name;]",
+            "textarea[0.5,2.0;9,7;file_content;Enter File Content;]",
+            "button_exit[7,9.3;2.5,0.9;create_file;create]"
+        }
+        local form = table.concat(formspec, "")
+        minetest.show_formspec(player_name, "directories:create_file", form)
+    end
 })
 
 minetest.register_tool("directories:spawn_rootdir", {
